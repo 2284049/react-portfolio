6379ca0658bc326aab2cb000d76d2b6d0c5133f3
2284049 <70497897+2284049@users.noreply.github.com>
2020-10-10 11:13:15 -0700

    add projects array

---
+++ projects.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    id: "a3927073-1af3-4939-ad38-47eb83f6aa42",
+    title: "Email and Password Validation of Matchmake Me",
+    rating: 4,
+    desc:
+      "Validation of email and password fields in Matchmake Me, following best security and UX practices.",
+    totalMinutes: 3,
+    postedAt: 1602352989438,
+    githubUrl: "https://github.com/2284049/matchmakeme",
+    image: "images/email-and-password-validation-of-matchmake-me.webp",
+    youtubeURL: "https://youtu.be/7u1h8cfvSJ0",
+    isActive: true,
+  },
+  {
+    id: "f37fdef3-ac94-40f2-a38c-04f969621b2e",
+    title: "Walkthrough of Matchmake Me Prototype",
+    rating: 5,
+    desc:
+      "Prototype of Matchmake Me, an app that bridges the divide between awful dating apps and expensive matchmaking services.",
+    totalMinutes: 3,
+    postedAt: 1602353001251,
+    githubUrl: "https://github.com/2284049/matchmakeme",
+    image: "images/walkthrough-of-matchmake-me-prototype.webp",
+    youtubeURL: "https://youtu.be/PdX0nalo0m4",
+    isActive: true,
+  },
+];
